94d265a35c377e5266ea7e0b7eaa748d27ff6dc9
yangtao <1093881953@qq.com>
2021-07-30 17:12:08 +0800

    171

--- 171/main.go
+++ 171/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+/**
+给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回该列名称对应的列序号。
+
+
+
+例如，
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28
+    ...
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/excel-sheet-column-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
 func main() {
 	columnTitle := "FXSHRXW"
 	fmt.Println(titleToNumber(columnTitle))
