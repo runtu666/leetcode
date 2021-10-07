5941959cb1a91fbc07c154ee1cceffab8ddc96dd
yangtao <1093881953@qq.com>
2021-10-07 10:23:24 +0800

    3

--- 3/main.go
+++ 3/main.go
@@ -3,59 +3,57 @@ package main
 import "fmt"
 
 /**
-3. 无重复字符的最长子串
-给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+给定一个字符串 s ，请你找出其中不含有重复字符的最长子串的长度。
 
-示例 1:
+
+
+示例1:
+
 输入: s = "abcabcbb"
 输出: 3
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-
 示例 2:
+
 输入: s = "bbbbb"
 输出: 1
 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-
 示例 3:
+
 输入: s = "pwwkew"
 输出: 3
-解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
-
+解释: 因为无重复字符的最长子串是"wke"，所以其长度为 3。
+    请注意，你的答案必须是 子串 的长度，"pwke"是一个子序列，不是子串。
 示例 4:
+
 输入: s = ""
 输出: 0
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
 func lengthOfLongestSubstring(s string) int {
-	var c = make(map[int32]struct{})
-	var leng, maxLen = 0, 0
-	for len(s) > 0 {
-		for _, i := range s {
-			if _, ok := c[i]; !ok {
-				c[i] = struct{}{}
-				leng += 1
-				if leng > maxLen {
-					maxLen = leng
-				}
-			} else {
-				c = make(map[int32]struct{})
-				c[i] = struct{}{}
-				leng = 1
+	var m = make(map[string]bool)
+	var index, cur, max int
+	cur = index
+	for length := len(s); index < length; index++ {
+		if m[string(s[index])] {
+			if len(m) > max {
+				max = len(m)
 			}
+			cur += 1
+			index = cur
+			m = make(map[string]bool)
 		}
-		s = s[1:]
-		if len(s) < maxLen {
-			break
-		}
-		c = make(map[int32]struct{})
-		leng = 0
+		m[string(s[index])] = true
 	}
-
-	return maxLen
+	if len(m) > max {
+		max = len(m)
+	}
+	return max
 }
 
 func main() {
-
-	fmt.Println(lengthOfLongestSubstring("asjrgapa"))
+	fmt.Println(lengthOfLongestSubstring("dvdf"))
 }
