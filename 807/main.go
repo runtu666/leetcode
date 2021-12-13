14a12a5fc0c5bf77f5f09d773ce4e57e88b36045
yangtao <1093881953@qq.com>
2021-12-13 10:41:25 +0800

    807

---
+++ 807/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+在二维数组grid中，grid[i][j]代表位于某处的建筑物的高度。 我们被允许增加任何数量（不同建筑物的数量可能不同）的建筑物的高度。 高度 0 也被认为是建筑物。
+
+最后，从新数组的所有四个方向（即顶部，底部，左侧和右侧）观看的“天际线”必须与原始数组的天际线相同。 城市的天际线是从远处观看时，由所有建筑物形成的矩形的外部轮廓。 请看下面的例子。
+
+建筑物高度可以增加的最大总和是多少？
+
+例子：
+输入： grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+输出： 35
+解释：
+The grid is:
+[ [3, 0, 8, 4],
+  [2, 4, 5, 7],
+  [9, 2, 6, 3],
+  [0, 3, 1, 0] ]
+
+从数组竖直方向（即顶部，底部）看“天际线”是：[9, 4, 8, 7]
+从水平水平方向（即左侧，右侧）看“天际线”是：[8, 7, 9, 3]
+
+在不影响天际线的情况下对建筑物进行增高后，新数组如下：
+
+gridNew = [ [8, 4, 8, 7],
+            [7, 4, 7, 7],
+            [9, 4, 8, 7],
+            [3, 3, 3, 3] ]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/max-increase-to-keep-city-skyline
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+	grid := [][]int{{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}}
+	fmt.Println(maxIncreaseKeepingSkyline(grid))
+}
+
+func maxIncreaseKeepingSkyline(grid [][]int) int {
+	var xMaxArr, yMaxArr []int
+	for x, arr := range grid {
+		var xMax, yMax int
+		for _, item := range arr {
+			if xMax < item {
+				xMax = item
+			}
+		}
+		xMaxArr = append(xMaxArr, xMax)
+		for y := 0; y < len(grid); y++ {
+			if yMax < grid[y][x] {
+				yMax = grid[y][x]
+			}
+		}
+		yMaxArr = append(yMaxArr, yMax)
+	}
+	var rs int
+	for x, xMax := range xMaxArr {
+		for y, yMax := range yMaxArr {
+			num := yMax
+			if xMax < yMax {
+				num = xMax
+			}
+			rs += num - grid[x][y]
+		}
+	}
+	return rs
+}
