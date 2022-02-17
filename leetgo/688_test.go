a2610b4b4b8a1b853c757bb75cf08e57ea22b611
yangtao <yangtao@qiniu.com>
2022-02-17 15:53:44 +0800

    688

---
+++ leetgo/688_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+)
+
+/**
+在一个n x n的国际象棋棋盘上，一个骑士从单元格 (row, column)开始，并尝试进行 k 次移动。行和列是 从 0 开始 的，所以左上单元格是 (0,0) ，右下单元格是 (n - 1, n - 1) 。
+
+象棋骑士有8种可能的走法，如下图所示。每次移动在基本方向上是两个单元格，然后在正交方向上是一个单元格。
+
+
+
+每次骑士要移动时，它都会随机从8种可能的移动中选择一种(即使棋子会离开棋盘)，然后移动到那里。
+
+骑士继续移动，直到它走了 k 步或离开了棋盘。
+
+返回 骑士在棋盘停止移动后仍留在棋盘上的概率 。
+
+
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/knight-probability-in-chessboard
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func Test688(t *testing.T) {
+	t.Log(knightProbability(3, 2, 0, 0))
+	t.Log(knightProbability(8, 30, 6, 0))
+}
+
+type Pos struct {
+	row    int
+	column int
+}
+
+var moves = []Pos{{1, 2}, {1, -2}, {2, 1}, {2, -1}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}}
+
+func knightProbability(n, k, row, column int) float64 {
+	dp := make([][][]float64, k+1)
+	for step := range dp {
+		dp[step] = make([][]float64, n)
+		for i := 0; i < n; i++ {
+			dp[step][i] = make([]float64, n)
+			for j := 0; j < n; j++ {
+				if step == 0 {
+					dp[step][i][j] = 1
+				} else {
+					for _, d := range moves {
+						if x, y := i+d.row, j+d.column; 0 <= x && x < n && 0 <= y && y < n {
+							dp[step][i][j] += dp[step-1][x][y] / 8
+						}
+					}
+				}
+			}
+		}
+	}
+	return dp[k][row][column]
+}
