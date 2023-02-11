27e6b1d31ae105b52ecc38be64951bc2a6809580
yangtao <mryang8614@163.com>
2023-02-11 09:41:43 +0800

    2335

---
+++ leetgo/2335_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"sort"
+	"testing"
+)
+
+func Test2335(t *testing.T) {
+
+}
+
+func fillCups(amount []int) int {
+	sort.Ints(amount)
+	if amount[2] > amount[1]+amount[0] {
+		return amount[2]
+	}
+	return (amount[0] + amount[1] + amount[2] + 1) / 2
+}
